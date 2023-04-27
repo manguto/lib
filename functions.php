7b088495f01c680bebf29dbe89535dd3f00f6000
marcosagtorres <marcosagtorres@gmail.com>
2023-04-27 13:23:04 -0300

    deb v2

--- functions.php
+++ functions.php
@@ -1,11 +1,8 @@
 <?php
-
-
-
-function deb($var, bool $die=true){
-	
-	echo "<pre>".var_dump($var)."</pre>";
-	
-	if($die) die();
-	
+function deb($var, bool $die = true){
+	echo "<pre>";
+	echo var_dump($var);
+	echo "</pre>";
+	if($die)
+		die();
 }
